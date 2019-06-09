f1a45d98524ed9211f5bc0feec2970c40125fc22
Harsha Uppala <harshauppala@gmail.com>
2019-06-09 13:43:58 -0400

    Create Week 9.R

---
+++ CodePortfolio/Week 9.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(dplyr)
+
+diamonds
+diamonds %>% 
+  count(cut)
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut))
+diamonds %>% 
+  count(cut_width(carat, 0.5))
+
+
+ggplot(data = diamonds) +
+  geom_histogram(mapping = aes(x = carat), binwidth = 0.5)
