75be3e3f06e3d23efd461eeba466bc98a2db5244
Harsha Uppala <harshauppala@gmail.com>
2019-06-09 12:59:41 -0400

    Update Week 4.R

--- CodePortfolio/Week 4.R
+++ CodePortfolio/Week 4.R
@@ -1,3 +1,13 @@
 chick <- ChickWeight
 
 summary(chick)
+
+read_csv("a,b,c
+1,2,3
+         4,5,6")
+
+str(parse_logical(c("TRUE", "FALSE", "NA")))
+
+parse_integer(c("1", "231", ".", "456"), na = ".")
+
+guess_parser("2010-10-01")
